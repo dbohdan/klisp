86f90ed12d18d29f39198a667cdcdc58e5fe3d83
Andres Navarro <canavarro82@gmail.com>
2011-04-18 19:34:29 -0300

    Bugfix: in kgeqp, eq? wasn't considering equal two applicatives with eq? underlying objects and two =? bigints.

--- src/kgeqp.h
+++ src/kgeqp.h
@@ -15,6 +15,8 @@
 
 #include "kstate.h"
 #include "kobject.h"
+#include "kapplicative.h" /* for unwrap */
+#include "kinteger.h" /* for kbigint_eqp */
 #include "klisp.h"
 #include "kghelpers.h"
 
@@ -23,11 +25,25 @@
 void eqp(klisp_State *K, TValue *xparams, TValue ptree, TValue denv);
 
 /* Helper (also used in equal?) */
-/* TEMP: for now this is the same as tv_equal,
-   later it will change with numbers and immutable objects */
+/* TEMP: this will change with immutable strings */
 inline bool eq2p(klisp_State *K, TValue obj1, TValue obj2)
 {
-    return (tv_equal(obj1, obj2));
+    bool res = (tv_equal(obj1, obj2));
+    if (!res && (ttype(obj1) == ttype(obj2))) {
+	if (ttisapplicative(obj1)) {
+	    while(ttisapplicative(obj1) && ttisapplicative(obj2)) {
+		obj1 = kunwrap(obj1);
+		obj2 = kunwrap(obj2);
+	    }
+	    res = (tv_equal(obj1, obj2));
+	} else if (ttisbigint(obj1)) {
+	    /* it's important to know that it can't be the case
+	       that obj1 is bigint and obj is some other type and
+	       (eq? obj1 obj2) */
+	    res = kbigint_eqp(obj1, obj2);
+	}
+    }
+    return res;
 }
 
 #endif
